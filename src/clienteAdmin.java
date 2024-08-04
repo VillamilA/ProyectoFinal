6de4f4a2fccf774d58e7585f4de8a31061703f66
VillamilA <angel.villamil@epn.edu.ec>
2024-08-04 15:07:28 -0500

    Creacion de Clientes en Admin

---
+++ src/clienteAdmin.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class clienteAdmin extends JFrame {
+
+    private JTabbedPane tabbedPane1;
+    private JTextField campoCedula;
+    private JTextField campoContra;
+    private JTextField campoUsuario;
+    private JPanel clientemenu;
+    private JButton crearClienteButton;
+    private JTabbedPane tabbedPane2;
+
+public clienteAdmin(){
+
+    crearClienteButton.addActionListener(new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+}
+
+
+
+}
