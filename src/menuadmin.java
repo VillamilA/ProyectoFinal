37c17bc06c1da4b3ed662a09e6ab7dea74ddc8fe
VillamilA <angel.villamil@epn.edu.ec>
2024-08-04 13:05:29 -0500

    Boton Salir menuadmin

--- src/menuadmin.java
+++ src/menuadmin.java
@@ -1,8 +1,19 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class menuadmin extends JFrame {
     private JButton clientesButton;
     private JButton trabajadoresButton;
     private JButton canchaButton;
     private JButton salirButton;
+
+    public menuadmin() {
+        salirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
 }
