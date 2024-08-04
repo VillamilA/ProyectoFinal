24843be9eb7d29c1e0beea240c7990784ac8c95a
VillamilA <angel.villamil@epn.edu.ec>
2024-08-04 12:35:32 -0500

    Creacion del menu admin

---
+++ src/menuadmin.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class menuadmin extends JFrame {
+    private JButton clientesButton;
+    private JButton trabajadoresButton;
+    private JButton canchaButton;
+    private JButton salirButton;
+}
